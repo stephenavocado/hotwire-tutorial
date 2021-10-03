72ca946efdc94d717d870a2214b9e577e38f79ef
stephenavocado <stephenwalter@chicagobooth.edu>
2021-10-03 21:35:15 +0000

    new discussion

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -10,6 +10,22 @@ class DiscussionsController < ApplicationController
   end
 
   def create
+    @discussion = Discussion.new(discussion_params)
+
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to discussions_path, notice: "Discussion created"}
+      else
+        format.html { render :new, status: :unprocessable_entity}
+      end
+    end
+
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:name, :closed, :pinned)
   end
 
 end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,3 +1,5 @@
+import "@hotwired/turbo-rails"
+
 import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
