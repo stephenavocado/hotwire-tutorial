016ce08743ee073083c06d182ebb0d0483efab12
stephenavocado <stephenwalter@chicagobooth.edu>
2021-10-03 19:33:46 +0000

    discussion model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :discussions, dependent: :destroy
+
   validates :username, presence: true, uniqueness: true
+  
 end
