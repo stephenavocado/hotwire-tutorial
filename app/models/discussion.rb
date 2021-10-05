ad6aff68f71fb98c7d2066845bb78453c027457f
stephenavocado <stephenwalter@chicagobooth.edu>
2021-10-05 01:27:24 +0000

    used to_param for discussion links

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -3,4 +3,9 @@ class Discussion < ApplicationRecord
 
   validates :name, presence: true
 
+  def to_param
+    "#{id}-#{name.downcase.to_s[0...100]}".parameterize
+    #.parameterize Replaces special characters in a string so that it may be used as part of a ‘pretty’ URL.
+  end
+
 end
