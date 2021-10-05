6b83ac02435a49cf6e988fa641c138d8e6c73d92
stephenavocado <stephenwalter@chicagobooth.edu>
2021-10-05 00:23:49 +0000

    removed dicsussion

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: [:index, :create, :new, :edit, :update]
+  resources :discussions, only: [:index, :create, :new, :edit, :update, :destroy]
   root to: "main#index"
 end
